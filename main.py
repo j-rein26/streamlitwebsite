05af3d6adb81d2082731d43af3f498ea6e87e6eb
j-rein26 <grow435@gmail.com>
2024-10-08 16:02:08 -0400

    Added ttitles, descriptions, images, and urls to the columns

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+import pandas 
 
 st.set_page_config(layout="wide")
 
@@ -31,3 +32,23 @@ Please feel free to contact me via the form below!
 """
 
 st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+	for index, row in df[:10].iterrows():
+		st.header(row["title"])
+		st.write(row["description"])
+		st.image("images/" + row["image"])
+		st.write(f"[Source Code]({row['url']})")
+
+
+with col4:
+	for index, row in df[10:].iterrows():
+		st.header(row["title"])
+		st.write(row["description"])
+		st.image("images/" + row["image"])
+		st.write(f"[Source Code]([{row['url']}])")
+
+
