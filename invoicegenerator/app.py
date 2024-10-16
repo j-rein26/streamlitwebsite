8dc466b8adf0947a16e95cef4632501473289348
j-rein26 <grow435@gmail.com>
2024-10-16 13:50:45 -0400

    add content for each animal category.

--- invoicegenerator/app.py
+++ invoicegenerator/app.py
@@ -22,6 +22,13 @@ for filepath in filepaths:
 	pdf.set_font(family="Times", size=16, style="B")
 	pdf.cell(w=50, h=8, txt=name, ln=1)
 
+	with open(filepath, "r") as file:
+		content = file.read()
+
+	#add text for each title to PDF
+	pdf.set_font(family="Times", size=12)
+	pdf.multi_cell(w=0, h=6, txt=content)
+
 
 pdf.output("animals.pdf")
 
